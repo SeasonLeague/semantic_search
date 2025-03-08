ccb2b8a2474d2d4173075cbb304dc015ba4d12a4
SeasonLeague <facebookups23@gmail.com>
2025-03-08 11:09:16 +0100

    [+] Vercel deployment issues fixing 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
@@ -10,10 +9,7 @@ const nextConfig = {
       },
     ],
   },
-  // Ensure we're not using any custom runtimes that might cause issues
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
+  experimental: {},
 }
 
 export default nextConfig
